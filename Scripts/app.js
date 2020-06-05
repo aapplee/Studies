4a9be0edafa8f73aaac441d394f867d827dc587f
Apple <64167365+aapplee@users.noreply.github.com>
2020-06-05 19:59:48 +0200

    Ez itten lenni a végső alakulat

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,21 +1,36 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('.time');
+const icon = document.querySelector('.icon img');
 
 
 const updateUI = (data) => {
-    const cityDets = data.cityDets;
-    const weather = data.weather;
+
+    const{cityDets, weather} = data
 
     //update details template
     details.innerHTML = `
-        <div class="text-muted text-uppercase text-center details">
-                    <h5 class="my-3">${cityDets.EnglishName}</h5>
-                    <div class="my-3">${weather.WeatherText}</div>
-                    <div class="display-4 my-4">
-                    <span>${weather.Temperature.Metric.Value}</span>
-                    <span>&deg;C</span>
+                    <div class="details">
+                    <div class="city-weather">
+                      <h3 class="city-name">${cityDets.EnglishName}</h3>
+                      <div class="weather-temp">${weather.WeatherText}</div>
+                        <span>${weather.Temperature.Metric.Value}</span>
+                        <span>&deg;C</span>
+                        </div>
+                        </div>
+                      </div>
     `;
+
+    // update daytime photo
+    let timeSrc = (weather.IsDayTime) ? 'img/day.svg': 'img/night.svg' ;
+    time.setAttribute('src', timeSrc);
+
+    // update icon
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`
+    icon.setAttribute('src', iconSrc)
+
+
     //remove display none
     if(card.classList.contains('d-none')) {card.classList.remove('d-none')};
 }
@@ -40,4 +55,13 @@ cityForm.addEventListener('submit', e => {
     // updateUI with new city
     updateCity(city).then(data => updateUI(data))
                     .catch(err => console.log(err));
+
+    //set local storage for the city 
+    localStorage.setItem('city', city);                   
 })
+
+if(localStorage.getItem('city')){
+  updateCity(localStorage.getItem('city'))
+  .then(data => updateUI(data))
+  .catch(err => console.log(err))
+}
